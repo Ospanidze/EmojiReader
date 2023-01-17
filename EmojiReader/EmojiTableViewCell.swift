601320747ad37b4e19784854cfa61384a373156c
SoloNineZero <solodyankin.ie@ya.ru>
2023-01-17 11:22:09 +0500

    Added custom TableVieCell

---
+++ EmojiReader/EmojiTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  EmojiTableViewCell.swift
+//  EmojiReader
+//
+//  Created by Игорь Солодянкин on 17.01.2023.
+//
+
+import UIKit
+
+class EmojiTableViewCell: UITableViewCell {
+    @IBOutlet weak var emojiLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+}
--- EmojiReader/EmojiTableViewController.swift
+++ EmojiReader/EmojiTableViewController.swift
@@ -18,8 +18,6 @@ class EmojiTableViewController: UITableViewController {
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         self.title = "Emoji Reader"
         self.navigationItem.leftBarButtonItem = self.editButtonItem
-        
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
     }
     
     // MARK: - Table view data source
@@ -36,9 +34,8 @@ class EmojiTableViewController: UITableViewController {
     
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = "\(indexPath)"
-        // Configure the cell...
+        let cell = tableView.dequeueReusableCell(withIdentifier: "emojiCell", for: indexPath) as! EmojiTableViewCell
+        cell.emojiLabel.text = "✋"
         
         return cell
     }
